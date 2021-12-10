0c55b9d4816460e8603792b8989713011ddc4f2d
siriwatknp <siriwatkunaporn@gmail.com>
2021-12-10 21:45:23 +0700

    update test

--- e2e/example.spec.mjs
+++ e2e/example.spec.mjs
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("basic test", async ({ page }) => {
+  // hello
   await page.goto("https://playwright.dev/");
   const title = page.locator(".navbar__inner .navbar__title");
   await expect(title).toHaveText("Playwright");
